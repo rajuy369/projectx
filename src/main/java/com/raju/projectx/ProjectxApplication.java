46e653f1b2ed57a8fe9a7d4137077a249e7e0ed8
rajuy369 <rsreddy137@gmail.com>
2018-09-14 19:49:01 -0700

    added new files

---
+++ src/main/java/com/raju/projectx/ProjectxApplication.java
@@ -0,0 +1,13 @@
+package com.raju.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectxApplication.class, args);
+		System.out.println("hello spring boot");
+	}
+}
